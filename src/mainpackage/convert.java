bdf48e3258cd977a0e8e2a9c433edd85b560a8b1
blohmeier <brett.lohmeier@gmail.com>
2020-03-19 06:52:17 -0500

    adding convert.java

---
+++ src/mainpackage/convert.java
@@ -0,0 +1,16 @@
+package mainpackage;
+
+public class convert {
+	public static void main(String[] args)
+	{
+		int age=40;
+		double salary=50000.09;
+		boolean isTrue=true;
+		
+		// convert to string
+		String NewSalary=String.valueOf(salary); //double to string
+		String NewAge=String.valueOf(age); //int to string
+		String NewisTrue=String.valueOf(isTrue); //boolean to string
+	}
+
+}
