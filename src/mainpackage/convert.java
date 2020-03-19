1d77c6d68347b086b18590c44151caad97c08e26
blohmeier <brett.lohmeier@gmail.com>
2020-03-19 07:33:47 -0500

    adding to 'convert' class

--- src/mainpackage/convert.java
+++ src/mainpackage/convert.java
@@ -6,11 +6,16 @@ public class convert {
 		int age=40;
 		double salary=50000.09;
 		boolean isTrue=true;
+		String rate="3.5"
 		
 		// convert to string
 		String NewSalary=String.valueOf(salary); //double to string
 		String NewAge=String.valueOf(age); //int to string
 		String NewisTrue=String.valueOf(isTrue); //boolean to string
+		
+		// convert to int
+		int new_rate=Integer.parseInt(rate);
+		int new_salary=
 	}
 
 }
